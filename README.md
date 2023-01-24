11830d66f038127b1a4fa969ee9bf4f2ba8a5c18
daveriesz <dave@riesz.net>
2023-01-23 20:39:40 -0800

    commits

---
+++ README.md
@@ -0,0 +1,78 @@
+# FE 5680A Serial Programmer
+
+This is a simple Bash script to manage the frequency of a FE-5680A rubidium
+frequency standard.  It can print the current frequency, set a new frequency,
+and persist the current freqency to NVRAM so that it is remembered if the
+unit's power is diconnected.
+
+It may also work with other FE-56xx devices.  I haven't investigated this.
+
+## Running
+
+Command-line usage can be obtained at any time with the ```-h```, ```--help```, or ```help``` arguments.  You will see the output:
+
+```
+Usage:
+
+  fe5680.sh help
+  fe5680.sh --help
+  fe5680.sh -help
+    Print this help message
+
+  fe5680.sh device <dev> [opt] <cmd ...>
+    Execute command(s) on a given FE56xx serial device, where:
+      "device <dev>" specifies the serial device
+      [opt] is an optional argument:
+         "raw"  print serial I/O from the device to stderr
+      <cmd> is one or more of the following:
+        "get"   print the current frequency
+        "set N" set the frequency to N Hz
+        "write" write the current frequency to NVRAM
+```
+
+Example 1:
+
+```
+  $ ./fe5680.sh device /dev/ttyUSB0 get
+```
+
+This prints the frequency of the FE-5680A device connected to serial port
+```/dev/ttyUSB0```.  It will output something like:
+
+```
+Frequency: 10,000,000.00 Hz
+```
+
+Example 2:
+
+```
+  $ ./fe5680.sh device /dev/ttyUSB0 get set 10123000.1 get
+```
+
+This prints the frequency of the FE-5680A device connected to serial port
+```/dev/ttyUSB0```, then sets the frequency to 10.1 MHz, then prints the
+frequency again.  It will output something like:
+
+```
+Frequency: 9,999,999.80 Hz
+Set frequency command succeeded.
+Frequency: 10,123,000.10 Hz
+```
+
+## Issues
+
+Issue?  Bug?  Feature request?  Please submit an [issue](issues).
+
+## Contributing
+
+Feel free to submit a pull request if you want to contribute.
+I'm not very good at reading other people's code, so please
+include a verbose description and/or explanation.
+
+## Licensing
+
+The script and attendant documentation are Copyright (C) 2023 David Riesz.
+
+This project and its code are all licensed under the MIT License.
+Please read the [license file](LICENSE).
+
---
+++ fe5680.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+# MIT License
+
+# Copyright (c) 2023 David Riesz
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+serialdev=
+
+status=
+ref=
+freq=
+
+doraw=0
+
+sendrecv()
+{
+  tempfile=$( mktemp -t fesh.XXXXXXXXXX )
+  exec 3<>$serialdev
+  stty -F $serialdev 9600 cs8 -cstopb -parenb #-ocrnl raw -echo
+  
+  cat <&3 >"$tempfile" &
+  catpid=$!
+  printf "%s\r" "$@" >&3
+  if [ $doraw -ne 0 ] ; then printf "wrote to serial:\n>>%s<<\n" "$@" >&2 ; fi
+  cont=1
+  while [ $cont -ne 0 ] ; do
+    sleep .1s
+    ok=$(cat "$tempfile" | tr "\015" "\n" | tail -1)
+    if [ "$ok" = "OK" ] ; then cont=0 ; fi
+  done
+
+  kill $catpid
+  wait $catpid 2>/dev/null
+  exec 3<&-
+  exec 3>&-
+
+  data=$(cat "$tempfile" | tr "\015" "\n")
+  if [ $doraw -ne 0 ] ; then printf "read from serial:\n>>%s<<\n" "$data" >&2 ; fi
+  printf "%s\n" "$data"
+
+  rm "$tempfile"
+}
+
+getstatus()
+{
+  status=$(sendrecv "S")
+  rr=$(echo "$status" | egrep "^R=" | sed 's/^R=\([0-9\.]*\)Hz F=\(........\)\(........\)$/\1/g')
+  f1=$(echo "$status" | egrep "^R=" | sed 's/^R=\([0-9\.]*\)Hz F=\(........\)\(........\)$/\2/g')
+  f2=$(echo "$status" | egrep "^R=" | sed 's/^R=\([0-9\.]*\)Hz F=\(........\)\(........\)$/\3/g')
+  ref="$rr"
+  freq=$(echo 3k 10o 16i $f1.$f2 Ai $rr \* 2 32^ / pq | dc -)
+}
+
+getfreq()
+{
+  getstatus
+  printf "Frequency: %'.2f Hz\n" "$freq"
+}
+
+setfreq()
+{
+  if ! echo "$1" | egrep "^([0-9]+|[0-9]+\.[0-9]+)$" >/dev/null ; then
+    echo "Invalid frequency: $1" >&2
+    echo "Frequency must be a whole number.\n" >&2
+    exit 1
+  fi
+  getstatus
+
+  val=$(echo 20k 10i 16o "$1" 2 32 ^ \* $ref / pq | dc - | sed 's/^\([0-9A-F]*\)\.\(........\).*$/\1\2/g')
+  data=$(sendrecv "F=$val")
+  if [ "$data" = OK ] ; then
+    echo "Set frequency command succeeded."
+  else
+    echo "Set frequency command failed." >&2
+  fi
+}
+
+write()
+{
+  data=$(sendrecv "E")
+  if [ "$data" = OK ] ; then
+    echo "Write frequency command succeeded."
+  else
+    echo "Write frequency command failed." >&2
+  fi
+}
+
+dohelp()
+{
+  (
+    bn=$(basename "$0")
+    printf "Usage:\n"
+    printf "\n"
+    printf "  $bn help\n"
+    printf "  $bn --help\n"
+    printf "  $bn -help\n"
+    printf "    Print this help message\n"
+    printf "\n"
+    printf "  $bn device <dev> [opt] <cmd ...>\n"
+    printf "    Execute command(s) on a given FE56xx serial device, where:\n"
+    printf "      \"device <dev>\" specifies the serial device\n"
+    printf "      [opt] is an optional argument:\n"
+    printf "         \"raw\"  print serial I/O from the device to stderr\n"
+    printf "      <cmd> is one or more of the following:\n"
+    printf "        \"get\"   print the current frequency\n"
+    printf "        \"set N\" set the frequency to N Hz\n"
+    printf "        \"write\" write the current frequency to NVRAM\n"
+    printf "\n"
+  ) >&2
+  exit 1
+}
+
+commands=( )
+
+while [ $# -gt 0 ] ; do
+  case "$1" in
+    device) shift ; serialdev="$1"                         ;;
+    get)            commands[${#commands[@]}]="getfreq"    ;;
+    set)    shift ; commands[${#commands[@]}]="setfreq $1" ;;
+    write)          commands[${#commands[@]}]="write"      ;;
+    raw)            doraw=1                                ;;
+    -h|--help|help) dohelp                                 ;;
+    *) echo "Unrecognized argument: $1" >&2 ; exit 1 ;;
+  esac
+  shift
+done
+
+if [ "$serialdev" = "" ] ; then
+  echo "No serial device specified." >&2
+  exit 1
+elif ! stty -F "$serialdev" >/dev/null 2>/dev/null ; then
+  echo "Cannot open serial device: $serialdev" >&2
+  exit 1
+fi
+
+for (( ii=0 ; ii<${#commands[@]} ; ii++ ))
+{
+  ${commands[$ii]}
+}
+
